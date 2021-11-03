f4ce39e783311ab6743cb3459d27566879690ddf
Matthew Pervykh <pervih.matvey@gmail.com>
2021-11-03 22:40:21 +0200

    Add NotesViewController

---
+++ KPIshnik/Controllers/NotesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NotesViewController.swift
+//  KPIshnik
+//
+//  Created by Matthew on 03.11.2021.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
