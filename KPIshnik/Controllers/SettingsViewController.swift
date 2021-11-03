f3c2c58de6a4f7928ba3cb181aaf6df77b870fac
Matthew Pervykh <pervih.matvey@gmail.com>
2021-11-03 22:40:49 +0200

    Add SettingsViewController

---
+++ KPIshnik/Controllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  KPIshnik
+//
+//  Created by Matthew on 03.11.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
