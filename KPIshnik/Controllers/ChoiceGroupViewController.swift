0ce68d93c3d82858374cfaafa88f2db136c9a822
Matthew Pervykh <pervih.matvey@gmail.com>
2021-11-04 22:40:27 +0200

    Add choice group view controller

---
+++ KPIshnik/Controllers/ChoiceGroupViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChoiceGroupViewController.swift
+//  KPIshnik
+//
+//  Created by Matthew on 04.11.2021.
+//
+
+import UIKit
+
+class ChoiceGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = CGSize(width:100 , height:100)
+    }
+    
+    @IBAction func close_view(){
+        
+       
+        
+    }
+    
+
+
+}
